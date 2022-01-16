7e2fcb4f5a84c1e486d70fcd1ea1cd6e710a2463
Akkashh Macha <akkashh.macha@visma.com>
2022-01-16 12:37:55 +0530

    Built in functions in Python | Ep2

---
+++ Python Basics Tutorial/first.py
@@ -0,0 +1,23 @@
+print('Hello world!')
+
+'''
+help
+input
+dir
+len
+min
+max
+abs
+pow
+'''
+
+help(print)
+name = input()
+
+print(name)
+print(dir(list))
+
+numbers = [1,2,3,4]
+print(len(numbers))
+
+print(pow(2,3))
